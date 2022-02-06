e199cf6c60c17ab2d445579cb5c4441530663384
Adam Piwoński <88802068+Madapiw@users.noreply.github.com>
2022-02-06 22:24:10 +0100

    init add libs

--- myFileMenager.py
+++ myFileMenager.py
@@ -1,9 +1,6 @@
 import time
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-
-
-
+from watchdog.events import FileSystemEventHandler 
 
 if __name__ == '__main__':
     pass
